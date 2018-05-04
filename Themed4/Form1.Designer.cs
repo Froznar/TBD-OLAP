84232304369cecd010eb30d21d1dd3ae3edfa57f
Francisco Marin <paracelso.silfos@hotmail.com>
2018-05-03 20:03:59 -0500

    Agregar archivos de proyecto.

---
+++ Themed4/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Themed4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.c1ThemeController1 = new C1.Win.C1Themes.C1ThemeController();
+            this.c1OlapPage1 = new C1.Win.Olap.C1OlapPage();
+            this.DimJer = new System.Windows.Forms.CheckedListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.c1ThemeController1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1OlapPage1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // c1OlapPage1
+            // 
+            this.c1OlapPage1.BackColor = System.Drawing.Color.White;
+            this.c1OlapPage1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.c1OlapPage1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.c1OlapPage1.Location = new System.Drawing.Point(0, 0);
+            this.c1OlapPage1.Margin = new System.Windows.Forms.Padding(2);
+            this.c1OlapPage1.Name = "c1OlapPage1";
+            this.c1OlapPage1.Size = new System.Drawing.Size(891, 570);
+            this.c1OlapPage1.TabIndex = 0;
+            this.c1ThemeController1.SetTheme(this.c1OlapPage1, "(default)");
+            this.c1OlapPage1.Load += new System.EventHandler(this.c1OlapPage1_Load);
+            // 
+            // DimJer
+            // 
+            this.DimJer.BackColor = System.Drawing.Color.White;
+            this.DimJer.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.DimJer.FormattingEnabled = true;
+            this.DimJer.Location = new System.Drawing.Point(584, 345);
+            this.DimJer.Name = "DimJer";
+            this.DimJer.Size = new System.Drawing.Size(307, 225);
+            this.DimJer.TabIndex = 1;
+            this.c1ThemeController1.SetTheme(this.DimJer, "(default)");
+            this.DimJer.SelectedIndexChanged += new System.EventHandler(this.DimJer_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(891, 570);
+            this.Controls.Add(this.DimJer);
+            this.Controls.Add(this.c1OlapPage1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.c1ThemeController1.SetTheme(this, "(default)");
+            ((System.ComponentModel.ISupportInitialize)(this.c1ThemeController1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1OlapPage1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private C1.Win.C1Themes.C1ThemeController c1ThemeController1;
+        private C1.Win.Olap.C1OlapPage c1OlapPage1;
+        private System.Windows.Forms.CheckedListBox DimJer;
+    }
+}
+
---
+++ Themed4/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Themed4.Properties;
+
+namespace Themed4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var jerarquias = new SqlDataAdapter("SELECT nombre FROM Jerarquia", GetConnectionString());
+            var dc = new DataTable();
+            jerarquias.Fill(dc);
+            //var manejador = c1FlexPivotPage1.FlexPivotEngine;
+            foreach (DataRow row in dc.Rows)
+            {
+                string name = row["nombre"].ToString();
+                DimJer.Items.Add(name);
+               // manejador.FilterFields.Add(name);
+            }
+
+            var dimensiones = new SqlDataAdapter("SELECT nombre FROM Hecho", GetConnectionString());
+            var dim = new DataTable();
+            dimensiones.Fill(dim);
+            foreach (DataRow row in dim.Rows)
+            {
+                string name = row["nombre"].ToString();                
+                DimJer.Items.Add(name);
+                //manejador.FilterFields.Add(name);
+            }
+            var ventas = new SqlDataAdapter("SELECT TOP 100 * FROM t_vst_ventas", GetConnectionString());
+            var ven = new DataTable();
+            ventas.Fill(ven);
+            c1OlapPage1.DataSource = ven;
+        }
+        static string GetConnectionString()
+        {
+            //Conecccion con la BD
+            return Settings.Default.MetadataTestConnectionString;
+        }
+
+        private void c1OlapPage1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DimJer_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string campos="";
+            for(int i = 0;i<DimJer.CheckedItems.Count;i++)
+            {
+                campos += DimJer.CheckedItems[i].ToString();
+                if(i!= DimJer.CheckedItems.Count-1) campos += ",";
+                Console.WriteLine(campos);
+            }
+            if(campos!="")
+            {
+                string consulta = "SELECT " + campos + " FROM t_vst_ventas";
+                Console.WriteLine(consulta);
+                var ventas = new SqlDataAdapter(consulta, GetConnectionString());
+                var ven = new DataTable();
+                ventas.Fill(ven);
+                c1OlapPage1.DataSource = ven;
+            }
+            
+
+        }
+
+    }
+    
+}
